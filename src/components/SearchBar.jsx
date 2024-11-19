34ab09b2a5fd9b30bdff7ead35998206379dc5b8
baibhav17 <baibhavsxn.035@gmail.com>
2024-11-19 18:31:49 +0530

    searchbar changes

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,19 +6,19 @@ import { setSearchTerm } from '../utils/State_Management/slices/dataSlice';
 import { genreTitle } from '../utils/constant';
 const SearchBar = () => {
     const dispatch = useDispatch();
-    // const [showBackBtn, setShowBackBtn] = useState(false)
     const [SearchBarText, setSearchBarText] = useState('')
     const [showSearchBar, setShowSearchBar] = useState(false)
   
     const handleSearchBackBtn = () => {
       setSearchBarText('')
       dispatch(setSearchTerm(''))
-      // setShowBackBtn(false)
       setShowSearchBar(false)
+      setSearchBarText('')
     }
 
     const handleShowSearchBar = () => {
       setShowSearchBar(!showSearchBar)
+      setSearchBarText('')
     }
     return (
       <div className='search-bar-wrapper'>
@@ -34,7 +34,6 @@ const SearchBar = () => {
         className='search-bar-input'
         value={SearchBarText}
         onChange={(e) => {
-          // e.target.value.length > 0 ? setShowBackBtn(true) : setShowBackBtn(false)
           setSearchBarText(e.target.value)
           dispatch(setSearchTerm(e.target.value))}
         }
