8685d925cf8e048d73ca02440dd888cf506d9308
baibhav17 <baibhavsxn.035@gmail.com>
2024-11-22 08:04:48 +0530

    fixed filtering

--- src/components/Thumbnail.jsx
+++ src/components/Thumbnail.jsx
@@ -1,12 +1,17 @@
 import '../styles/Thumbnail.css'
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import imgNotFound from '../assets/imgNotFound.png';
 const Thumbnail = React.forwardRef(({ title, imageUrl }, ref) => {
   const [currentImageUrl, setCurrentImageUrl] = useState(imageUrl);
-  const fallbackImage = imgNotFound;
+
+// Updating the image whenever filtering is done.
+  useEffect(()=>{
+    setCurrentImageUrl(imageUrl)
+  },[imageUrl])
+
   // If thumbnail image is not found then this image will be rendered. This img is from the API.
   const handleImageError = () => {
-    setCurrentImageUrl(fallbackImage);
+    setCurrentImageUrl(imgNotFound);
   };
 
   return (
